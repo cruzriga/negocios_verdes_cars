36eeba452660574fe9362191e494dabb9799ce5a
Cruz Rivera <cruz.riga@hotmail.com>
2021-03-12 02:23:08 -0500

    menu

--- administrator/components/com_mrnegociosverde/js/admin.js
+++ administrator/components/com_mrnegociosverde/js/admin.js
@@ -89,32 +89,11 @@ __webpack_require__.r(__webpack_exports__);
       left: true,
       menuList: [{
         icon: 'inbox',
-        label: 'Inbox',
+        label: 'Negocios Verdes',
         separator: true
       }, {
         icon: 'send',
-        label: 'Outbox',
-        separator: false
-      }, {
-        icon: 'delete',
-        label: 'Trash',
-        separator: false
-      }, {
-        icon: 'error',
-        label: 'Spam',
-        separator: true
-      }, {
-        icon: 'settings',
-        label: 'Settings',
-        separator: false
-      }, {
-        icon: 'feedback',
-        label: 'Send Feedback',
-        separator: false
-      }, {
-        icon: 'help',
-        iconColor: 'primary',
-        label: 'Help',
+        label: 'Categorias',
         separator: false
       }]
     };
--- resources/admin/js/layouts/layout.vue
+++ resources/admin/js/layouts/layout.vue
@@ -51,38 +51,12 @@ export default {
         menuList : [
           {
             icon: 'inbox',
-            label: 'Inbox',
+            label: 'Negocios Verdes',
             separator: true
           },
           {
             icon: 'send',
-            label: 'Outbox',
-            separator: false
-          },
-          {
-            icon: 'delete',
-            label: 'Trash',
-            separator: false
-          },
-          {
-            icon: 'error',
-            label: 'Spam',
-            separator: true
-          },
-          {
-            icon: 'settings',
-            label: 'Settings',
-            separator: false
-          },
-          {
-            icon: 'feedback',
-            label: 'Send Feedback',
-            separator: false
-          },
-          {
-            icon: 'help',
-            iconColor: 'primary',
-            label: 'Help',
+            label: 'Categorias',
             separator: false
           }
         ]
