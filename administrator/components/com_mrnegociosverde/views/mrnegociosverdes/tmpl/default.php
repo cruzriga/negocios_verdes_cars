2618a149ef48bc3dbc56ae4d32892a7e73a51a2b
CRUZRIGA <cruz.riga@hotmail.com>
2021-03-31 08:35:21 -0500

    Update default.php

--- administrator/components/com_mrnegociosverde/views/mrnegociosverdes/tmpl/default.php
+++ administrator/components/com_mrnegociosverde/views/mrnegociosverdes/tmpl/default.php
@@ -1,12 +1,12 @@
 <?php JToolBarHelper::title("Mr Negocios Verdes") ?>
 <div id="app-mr-negocios-verdes">
     <script>
-        const  base_url  = '<?=JURI::base()?>';
+        const  base_url  = '<?=JURI::base(true)?>';
     </script>
 
     <link href="<?=JURI::base()?>components/com_mrnegociosverde/css/admin.css" rel="stylesheet">
     <div id="appadmin"></div>
-    <script src="<?=JURI::base()?>components/com_mrnegociosverde/js/admin.js?v=1.3"></script>
+    <script src="<?=JURI::base()?>components/com_mrnegociosverde/js/admin.js?v=1.?v=2.<?= time()?>"></script>
 </div>
 <style>
     .subhead-collapse{
